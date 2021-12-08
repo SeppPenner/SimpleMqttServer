737ee282827cb31788788cb17c0b38b159b8b661
SeppPenner <HansM2013@gmx.de>
2021-12-08 07:37:28 +0100

    Added setup for ARM image.

--- buildAndUploadDocker.bat
+++ buildAndUploadDocker.bat
@@ -1,5 +1,5 @@
 cd src\SimpleMqttServer
-dotnet publish -c Release --output publish/
+dotnet publish -c Release --output publish/ -r linux --no-self-contained
 docker build --tag sepppenner/simplemqttserver:1.0.3 -f Dockerfile .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
 docker push sepppenner/simplemqttserver:1.0.3
---
+++ buildAndUploadDockerForArm.bat
@@ -0,0 +1,7 @@
+cd src\SimpleMqttServer
+dotnet publish -c Release --output publish/ -r linux-arm --no-self-contained
+docker build --tag sepppenner/simplemqttserver-arm:1.0.3 -f Dockerfile.armv7 .
+docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
+docker push sepppenner/simplemqttserver-arm:1.0.3
+@ECHO.Build successful. Press any key to exit.
+pause
