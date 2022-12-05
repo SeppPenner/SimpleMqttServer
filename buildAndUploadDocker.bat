66322a53fa0bad13772369fb0638b8e8cf0c7c6e
SeppPenner <HansM2013@gmx.de>
2022-12-05 16:26:01 +0100

    Updated Docker build scripts.

--- buildAndUploadDocker.bat
+++ buildAndUploadDocker.bat
@@ -1,7 +1,7 @@
 cd src\SimpleMqttServer
 dotnet publish -c Release --output publish/ -r linux-x64 --no-self-contained
-docker build --tag sepppenner/simplemqttserver:1.0.5 -f Dockerfile .
+docker build --tag sepppenner/simplemqttserver:1.0.6 -f Dockerfile .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
-docker push sepppenner/simplemqttserver:1.0.5
+docker push sepppenner/simplemqttserver:1.0.6
 @ECHO.Build successful. Press any key to exit.
 pause
--- buildAndUploadDockerForArm.bat
+++ buildAndUploadDockerForArm.bat
@@ -1,7 +1,7 @@
 cd src\SimpleMqttServer
 dotnet publish -c Release --output publish/ -r linux-arm --no-self-contained
-docker build --tag sepppenner/simplemqttserver-arm:1.0.5 -f Dockerfile.armv7 .
+docker build --tag sepppenner/simplemqttserver-arm:1.0.6 -f Dockerfile.armv7 .
 docker login -u sepppenner -p "%DOCKERHUB_CLI_TOKEN%"
-docker push sepppenner/simplemqttserver-arm:1.0.5
+docker push sepppenner/simplemqttserver-arm:1.0.6
 @ECHO.Build successful. Press any key to exit.
 pause
